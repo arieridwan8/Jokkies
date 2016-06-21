05d9fcd8265e4318e3784addfc751295f054702d
Arie Ridwansyah <arieridwansyah@gmail.com>
2016-06-21 10:48:57 +0700

    Adding final code from friendly chat

--- app/src/main/java/id/arieridwan/jokkies/preference/Preferences.java
+++ app/src/main/java/id/arieridwan/jokkies/preference/Preferences.java
@@ -4,5 +4,6 @@ package id.arieridwan.jokkies.preference;
  * Created by root on 21/06/16.
  */
 public class Preferences {
+    public static final String INSTANCE_ID_TOKEN_RETRIEVED = "iid_token_retrieved";
     public static final String FRIENDLY_MSG_LENGTH = "friendly_msg_length";
 }
